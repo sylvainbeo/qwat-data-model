2ee7e0c60a555b48b2bc66a2d4e106b0ae7e6ddf
Sylvain Beorchia <sylvain.beorchia@oslandia.com>
2017-03-21 15:21:52 +0100

    Missing order by in migration SQL

--- update/test_migration.py
+++ update/test_migration.py
@@ -62,10 +62,10 @@ def _execute_statements(cur, fileName, diff_exe):
         
         # For debug purposes
         print "=========================================================================="
-        print ".........................................................................."
         print buffer_expected.replace('%','') # TODO Debug purpose
         print "--------------------------------------------------------------------------"
         print buffer.replace('%','') # TODO Debug purpose
+        print ".........................................................................."
         # os.system('{diff} --ignore-all-space --ignore-blank-lines --side-by-side test_migration.expected.sql output.sql'.format(diff=diff_exe))
 
 if __name__ == "__main__":
